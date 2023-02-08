069cb2847ecc45104f2a479fc4978238a13b8394
unbxd <unbxd@unbxds-MacBook-Air.local>
2023-02-08 16:15:46 +0530

    updated version

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,15 +6,15 @@ from flask_marshmallow import Marshmallow
 from sqlalchemy.engine import result
 ma=Marshmallow()
 db=SQLAlchemy()
-from .models import website_data,ProdSchema
-# DB_NAME='test1.db'
+from .models import website_data2,ProdSchema
+# DB_NAME='test1.db's
 
-engine=create_engine('postgresql://postgres:12345@localhost:5432/test1')
+engine=create_engine('postgresql://postgres:12345@localhost:5432/db2')
 
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='thisisthekey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/test1'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/db2'
     db.init_app(app)
 
     from .views import views
@@ -23,7 +23,6 @@ def create_app():
     app.register_blueprint(views,url_prefix='/')
     # app.register_blueprint(prod_bp,url_prefix='/prod_bp')
     app.register_blueprint(auth,url_prefix='/')
-    
     
     return app
 
--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from . import db,ma
 from sqlalchemy.orm import class_mapper
 
 
-class website_data(db.Model):
+class website_data2(db.Model):
     color = db.Column(db.String)
     categoryType = db.Column(db.String)
     productUrl = db.Column(db.String)
@@ -20,6 +20,7 @@ class website_data(db.Model):
     gender=db.Column(db.String)
     catlevel4Name=db.Column(db.String)
     uniqueId=db.Column(db.String,primary_key=True)
+    productImage=db.Column(db.String)
 
 class ProdSchema(ma.Schema):
     class Meta:
@@ -27,5 +28,5 @@ class ProdSchema(ma.Schema):
         #fields=('color','categoryType','productUrl','availability','size','category','productDescription','catlevel2Name','title','sku','price','catlevel3Name','name','gender','catlevel4Name','uniqueId')
 product_schema = ProdSchema()
 products_schema = ProdSchema(many=True)
-mapper=class_mapper(website_data)
+mapper=class_mapper(website_data2)
 test1=mapper.mapped_table
